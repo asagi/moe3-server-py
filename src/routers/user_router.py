a2d85b786f9189692bf5f23b31dbd5b8864b7ea1
Keiju Asagi <asagi@tepidworks.jp>
2024-12-26 21:56:56 +0900

    chore(lint): add flake8 rules

--- src/routers/user_router.py
+++ src/routers/user_router.py
@@ -9,22 +9,22 @@ user_router = APIRouter()
 
 
 @user_router.get("/users")
-async def list_users():
+async def list_users() -> dict:
     users = Depends(get_db).query(User).all()
     return {"data": users}
 
 
 @user_router.post("/users")
-async def create_user(data: dict, db: Session = Depends(get_db)):
+async def create_user(data: dict, db: Session = Depends(get_db)) -> dict:
     user = UserCreate(**data)
     return create_user_entity(db=db, user=user)
 
 
 @user_router.put("/users/{user_id}")
-async def update_user():
+async def update_user() -> dict:
     pass
 
 
 @user_router.delete("/users/{user_id}")
-async def delete_user():
+async def delete_user() -> dict:
     pass
