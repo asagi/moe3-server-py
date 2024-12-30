da87f838715264de6950e329eb9483b9e936842a
Keiju Asagi <asagi@tepidworks.jp>
2024-12-30 18:41:48 +0900

    feat(model): improve province

--- src/models/province_model.py
+++ src/models/province_model.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from models.base_model import Base
 
 
@@ -10,6 +11,10 @@ class Province(Base):
     abbr = Column("abbr", String)
     name = Column("name", String)
     jname = Column("jname", String)
+    supply_center = Column("supply_center", Boolean, default=False)
+    power_id = Column(Integer, ForeignKey("powers.id"))
+
+    region = relationship("Power", uselist=False)
 
     __mapper_args__ = {
         "polymorphic_identity": "province",
--- tests/test_unit_model.py
+++ tests/test_unit_model.py
@@ -1,11 +1,11 @@
 from models.power_model import Power
-from models.province_model import Coast
+from models.province_model import Province
 from models.unit_model import Unit, Army, Fleet
 
 
 def test_army_creation(master_data):
     power = master_data.query(Power).filter_by(symbol="a").first()
-    province = Coast(abbr="lon")
+    province = master_data.query(Province).filter_by(abbr="lon").first()
     new_unit = Army(province=province, power=power)
     master_data.add(new_unit)
     master_data.commit()
@@ -18,7 +18,7 @@ def test_army_creation(master_data):
 
 def test_fleet_creation(master_data):
     power = master_data.query(Power).filter_by(symbol="g").first()
-    province = Coast(abbr="lon")
+    province = master_data.query(Province).filter_by(abbr="lon").first()
     new_unit = Fleet(province=province, power=power)
     master_data.add(new_unit)
     master_data.commit()
