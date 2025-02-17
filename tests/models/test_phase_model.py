95f7feb0f0161c9ba594606d3680162c9a28e678
Keiju Asagi <asagi@tepidworks.jp>
2025-02-17 14:50:25 +0900

    feat(model): add method phase#end

--- tests/models/test_phase_model.py
+++ tests/models/test_phase_model.py
@@ -1,3 +1,4 @@
+# pyright: reportPrivateUsage=false
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.game_table_model import GameTable
@@ -22,7 +23,7 @@ async def test_phase_relation(master_data: AsyncSession) -> None:
     first_phase = new_table.phases[0]
     assert first_phase is not None
     assert first_phase.prev_phase is None
-    second_phase = first_phase.create_next_phase()
+    second_phase = first_phase._create_next_phase()
     assert second_phase is not None
     assert second_phase.prev_phase == first_phase
     assert second_phase.year == 1901
@@ -36,7 +37,7 @@ async def test_phase_status(master_data: AsyncSession) -> None:
     first_phase = new_table.phases[0]
     assert first_phase is not None
     assert first_phase.status == Phase.Status.OPEN
-    second_phase = first_phase.create_next_phase()
+    second_phase = first_phase._create_next_phase()
     assert first_phase is not None
     assert first_phase.status == Phase.Status.CLOSED
     assert second_phase is not None
@@ -49,16 +50,16 @@ async def test_phase_year(master_data: AsyncSession) -> None:
     first_phase = new_table.phases[0]
     assert first_phase is not None
     assert first_phase.year == 0
-    second_phase = first_phase.create_next_phase()
+    second_phase = first_phase._create_next_phase()
     assert second_phase is not None
     assert second_phase.year == 1901
-    third_phase = second_phase.create_next_phase()
-    fourth_phase = third_phase.create_next_phase()
-    fifth_phase = fourth_phase.create_next_phase()
-    sixth_phase = fifth_phase.create_next_phase()
+    third_phase = second_phase._create_next_phase()
+    fourth_phase = third_phase._create_next_phase()
+    fifth_phase = fourth_phase._create_next_phase()
+    sixth_phase = fifth_phase._create_next_phase()
     assert sixth_phase is not None
     assert sixth_phase.year == 1901
-    seventh_phase = sixth_phase.create_next_phase()
+    seventh_phase = sixth_phase._create_next_phase()
     assert seventh_phase is not None
     assert seventh_phase.type == "spring_order"
     assert seventh_phase.year == 1902
@@ -69,7 +70,7 @@ async def test_phase_order_initialization(master_data: AsyncSession) -> None:
     new_table = await GameTable.create_with_phases(new_user)
     first_phase = new_table.phases[0]
     assert first_phase is not None
-    second_phase = first_phase.create_next_phase()
+    second_phase = first_phase._create_next_phase()
     assert second_phase is not None
     assert len(first_phase.latest_units) == 22
     assert len(first_phase.orders) == 0
