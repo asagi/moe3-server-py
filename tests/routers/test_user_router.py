1d237f27222b4a1550211be0a2dfffb0604afe28
Keiju Asagi <k.asagi@nifty.com>
2025-04-01 11:19:37 +0900

    feat(router): add allow unauthorized decorator

--- tests/routers/test_user_router.py
+++ tests/routers/test_user_router.py
@@ -9,6 +9,7 @@ from httpx import Response
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.database import get_db
 from models.user_model import User
 
 
@@ -20,12 +21,6 @@ def mock_user() -> Generator[MagicMock, Any, None]:
     mock_response.data.name = "dname"
     mock_response.data.username = "sname"
     yield mock_response
-
-
-async def test_read_hello(client: TestClient) -> None:
-    response: Response = client.get("/hello")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "7 powers"}
 
 
 async def test_login_new_user(client: TestClient, mock_user: MagicMock) -> None:
@@ -68,6 +63,7 @@ async def test_login_exist_user(client: TestClient, master_data: AsyncSession, m
         with patch("services.user_service._generate_access_key", return_value="accesskey"):
             with patch("services.user_service._get_current_time", return_value=dt_utc):
                 response: Response = client.post("/users", json={"access_token": "abc"})
+
     assert response.status_code == 200
     assert response.json() == {
         "xid": 123,
@@ -77,7 +73,8 @@ async def test_login_exist_user(client: TestClient, master_data: AsyncSession, m
         "last_access_time": "2025-03-31T03:54:50.166954Z",
     }
 
-    updated_user: User | None = (await master_data.execute(select(User).filter_by(xid=123))).scalar_one_or_none()
+    db: AsyncSession = await get_db().__anext__()
+    updated_user: User | None = (await db.execute(select(User).filter_by(xid=123))).scalar_one_or_none()
     assert updated_user is not None
     assert updated_user.screen_name == "sname"
     assert updated_user.display_name == "dname"
