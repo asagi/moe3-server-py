bda12a62f811ca783bae3ba849d9ad119438ae6d
Keiju Asagi <k.asagi@nifty.com>
2025-04-02 12:59:04 +0900

    feat(model): add property power to user

--- src/models/player_model.py
+++ src/models/player_model.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from models.base_model import Base
 from models.game_table_model import GameTable
+from models.power_model import Power
 from models.user_model import User
 
 
@@ -12,9 +13,11 @@ class Player(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
     table_id: Mapped[int] = mapped_column(Integer, ForeignKey("game_tables.id"))
+    power_id: Mapped[int | None] = mapped_column(Integer, ForeignKey("powers.id"))
 
     user: Mapped[User] = relationship("User", foreign_keys=[user_id], back_populates="players", uselist=False)
     table: Mapped[GameTable] = relationship("GameTable", foreign_keys=[table_id], back_populates="players", uselist=False)
+    power: Mapped["Power | None"] = relationship("Power", foreign_keys=[power_id], uselist=False)
 
     def __init__(self, user: User, table: GameTable) -> None:
         self.user = user
