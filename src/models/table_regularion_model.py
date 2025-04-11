3ad9d7c33565cec26848fe3ede64d42825460cf4
Keiju Asagi <asagi@tepidworks.jp>
2025-04-11 18:07:12 +0900

    feat(model): add table_regulation class

---
+++ src/models/table_regularion_model.py
@@ -0,0 +1,42 @@
+import enum
+from datetime import datetime
+
+from sqlalchemy import Enum, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+from models.base_model import Base
+from value_objects.duration_values import (
+    Duration,
+    fixed_middle_duration,
+    flex_short_duration,
+)
+
+
+class TableRegulation(Base):
+    __tablename__ = "table_regulations"
+
+    class FaceType(enum.Enum):
+        GIRLS = 1
+        FLAGS = 2
+
+    class DurationType(enum.Enum):
+        FIXED_MIDDLE = 1
+        FLEX_SHORT = 2
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    face_type: Mapped[FaceType] = mapped_column(Enum(FaceType), nullable=False)
+    duration_type: Mapped[DurationType] = mapped_column(Enum(DurationType), nullable=False)
+    start_time: Mapped[datetime] = mapped_column(nullable=False)
+
+    _duration: Duration
+
+    def __init__(self, face_type: FaceType, duration_type: DurationType, start_time: datetime) -> None:
+        self.face_type = face_type
+        self.duration_type = duration_type
+        self.start_time = start_time
+
+        match duration_type:
+            case self.DurationType.FIXED_MIDDLE:
+                self._duration = fixed_middle_duration
+            case self.DurationType.FLEX_SHORT:
+                self._duration = flex_short_duration
