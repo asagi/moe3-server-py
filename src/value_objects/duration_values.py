6cf7d8af08049a20dc34e1a177510748187db9e7
Keiju Asagi <k.asagi@nifty.com>
2025-04-11 13:52:41 +0900

    feat(value_object): add duration value objects (#37)
    * feat(value_object): add duration value objects
    
    * chore(poetry): update pyright
    
    * fix(value_object): correct due_mode of fixed_middle_duration
    
    ---------
    
    Co-authored-by: Keiju Asagi <asagi@tepidworks.jp>

---
+++ src/value_objects/duration_values.py
@@ -0,0 +1,32 @@
+import enum
+from typing import NamedTuple
+
+
+class DueMode(enum.Enum):
+    FIXED = 1
+    FLEXIBLE = 2
+
+
+class Duration(NamedTuple):
+    due_mode: DueMode
+    order_phase: int
+    retreat_phase: int
+    adjustment_phase: int
+    debrief_phase: int
+
+
+flex_short_duration = Duration(
+    due_mode=DueMode.FLEXIBLE,
+    order_phase=60,
+    retreat_phase=10,
+    adjustment_phase=10,
+    debrief_phase=60 * 24,
+)
+
+fixed_middle_duration = Duration(
+    due_mode=DueMode.FIXED,
+    order_phase=60 * 24,
+    retreat_phase=60,
+    adjustment_phase=60,
+    debrief_phase=60 * 24,
+)
