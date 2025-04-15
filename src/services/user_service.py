9f94a5fe33c73341b06ae98edad2428d7c67842a
Keiju Asagi <k.asagi@nifty.com>
2025-04-15 13:14:11 +0900

    refactor(service): replace current time getter in user service

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,5 +1,4 @@
 import uuid
-from datetime import datetime, timezone
 from typing import cast
 
 from google.oauth2.credentials import Credentials
@@ -7,16 +6,13 @@ from googleapiclient.discovery import build  # type: ignore
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.util import get_current_time
 from models.user_model import User
 from schemas.user_schema import UserLogin
 
 
 def _generate_access_key():
     return str(uuid.uuid4())
-
-
-def _get_current_time():
-    return datetime.now(timezone.utc)
 
 
 async def login_user(db: AsyncSession, param: UserLogin) -> User:
@@ -33,13 +29,13 @@ async def login_user(db: AsyncSession, param: UserLogin) -> User:
         user.picture = picture
         user.gname = gname
         user.access_key = _generate_access_key()
-        user.last_access_time = _get_current_time()
+        user.last_access_time = get_current_time()
         await db.commit()
         return user
 
     new_user = User(gid=gid, gname=gname, picture=picture)
     new_user.access_key = _generate_access_key()
-    new_user.last_access_time = _get_current_time()
+    new_user.last_access_time = get_current_time()
     db.add(new_user)
     await db.commit()
     return new_user
@@ -48,7 +44,7 @@ async def login_user(db: AsyncSession, param: UserLogin) -> User:
 async def update_last_access_time(db: AsyncSession, token: str) -> None:
     user: User = (await db.execute(select(User).filter_by(access_key=token))).scalar_one_or_none()
     if user:
-        user.last_access_time = _get_current_time()
+        user.last_access_time = get_current_time()
         await db.commit()
     else:
         raise Exception("User not found")
